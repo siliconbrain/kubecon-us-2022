3467e1f55ebb79410f4d649f009ae2bc6c64bd53
Dudás Ádám <adudas@cisco.com>
2022-09-29 10:11:24 +0200

    use public function names

--- plugins/mutatejson/main.go
+++ plugins/mutatejson/main.go
@@ -2,25 +2,25 @@ package main
 
 //go:wasm-module env
 //export error
-func error(ptr *byte, len uint)
+func Error(ptr *byte, len uint)
 
 //go:wasm-module env
 //export get_data
-func get_data(ptr *byte)
+func GetData(ptr *byte)
 
 //go:wasm-module env
 //export send
-func send(ptr *byte, len uint) bool
+func Send(ptr *byte, len uint) bool
 
 //export receive
-func receive(dataLen uint) {
+func Receive(dataLen uint) {
 	data := make([]byte, dataLen)
-	get_data(&data[0])
+	GetData(&data[0])
 	runes := []rune(string(data))
 	for i := 0; i < len(runes)/2; i++ {
 		j := len(runes) - 1 - i
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	data = []byte(string(runes))
-	send(&data[0], uint(len(data)))
+	Send(&data[0], uint(len(data)))
 }
